54802a35f4c0a48cee85446a35e48a3a2fac2b7a
Park JeongTae <pjt3591oo@gmail.com>
2016-02-12 16:19:24 +0900

    Database Configure Add

---
+++ dbconf/dbconf.js
@@ -0,0 +1,12 @@
+﻿var mysql = require('./node_modules/mysql');
+
+exports.dbconf=function(dbinfo) {
+
+    return mysql.createConnection({
+	host:'__hostname',
+	port:'__port',
+        user: '__username',
+        password: '__password',
+        database: '__databasename'
+    });
+};
