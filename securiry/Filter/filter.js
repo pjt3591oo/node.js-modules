41c78a4568158c5748aca36d3621c3308697d390
Park JeongTae <pjt3591oo@gmail.com>
2016-02-15 19:15:28 +0900

    filter modules add

---
+++ securiry/Filter/filter.js
@@ -0,0 +1,25 @@
+﻿/*
+  * 해당 모듈은 XSS,SQLInjection 공격을 막기 위한 필터링 모듈입니다
+ */
+
+exports.TextFilter = function(text){
+   // text = text.toLowerCase();
+
+    //특수문자 필터
+    text=text.replace('<', "&lt;");
+    text=text.replace('>', "&gt;");
+    text=text.replace('"', "&quot;");
+    text=text.replace('=', "&nbsp;");
+    text=text.replace('?', "&#63;");
+    text=text.replace("'", "&#44;");
+    text=text.replace('\\', "&#92;");
+    text=text.replace(';', "&#59;");
+    text=text.replace('%', "&#37;");
+    text=text.replace('-', "&#45;");
+    text=text.replace('(', "&#40;");
+    text=text.replace(')', "&#41;");
+    text=text.replace('/', "&#47;");
+
+
+    return text;
+};
---
+++ securiry/Filter/test.js
@@ -0,0 +1,3 @@
+﻿var f = require('./filter.js');
+
+console.log(f.TextFilter('asdasdasdasdf3$#*&#$JASDJ(#(9'));
